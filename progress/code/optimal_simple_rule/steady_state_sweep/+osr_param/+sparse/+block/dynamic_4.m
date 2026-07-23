5b76fd3dd605ca0ee10ed37435c67a99a80e340c
Kohsuke KISHIMOTO <kishimoto.research@gmail.com>
2026-07-24 01:04:21 +0900

    消す

--- progress/code/optimal_simple_rule/steady_state_sweep/+osr_param/+sparse/+block/dynamic_4.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = dynamic_4(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(39)=y(2)*(params(6)+params(5))+params(11)*(params(7)+y(8))^params(4);
-end
