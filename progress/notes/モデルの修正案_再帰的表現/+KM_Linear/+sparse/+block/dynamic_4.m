ffe0049395012c5be6ff1ab7f91db694aba332dc
Kohsuke KISHIMOTO <kishimoto.research@gmail.com>
2026-07-24 00:34:31 +0900

    修正

--- progress/notes/モデルの修正案_再帰的表現/+KM_Linear/+sparse/+block/dynamic_4.m
+++
@@ -1,4 +0,0 @@
-function [y, T] = dynamic_4(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(19)=y(16)+params(2)*y(17);
-  y(20)=y(19);
-end
--- progress/notes/モデルの修正案_再帰的表現/+KM_Linear/+sparse/+block/static_4.m
+++
@@ -1,4 +0,0 @@
-function [y, T] = static_4(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(9)=y(6)+params(2)*y(7);
-  y(10)=y(9);
-end
